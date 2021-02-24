f48fd200d9ac74e436a14d8c519bc10016ad819a
Gracielle Oliveira <gra2gso@gmail.com>
2021-02-24 16:57:45 -0300

    Incrementação de entradas e saídas em JavaScript

--- scripts.js
+++ scripts.js
@@ -14,59 +14,140 @@ const Modal = {
 };
 
 // Lista de objetos
-const transaction = [
+const transactions = [
   {
     id: 1,
-    description: 'Luz',
+    description: "Luz",
     amount: -50000,
     date: "23/01/2021",
   },
   {
     id: 2,
-    description: 'Website',
+    description: "Website",
     amount: 500000,
     date: "23/01/2021",
-},
-  {id: 3,
-    description: 'Internet',
+  },
+
+  {
+    id: 3,
+    description: "Internet",
     amount: -20000,
-    date: "23/01/2021",},
+    date: "23/01/2021",
+  },
+
+  {
+    id: 4,
+    description: "App",
+    amount: 200000,
+    date: "23/01/2021",
+  },
 ];
 
-
+// Responsável pelo cálculo matemático
 const Transaction = {
   incomes() {
-    //somar as entradas
-  },
+    let income = 0
+    transactions.forEach(transaction => {
 
+      if (transaction.amount > 0){
+        income += transaction.amount;
+      }
+    })
+
+    return income;
+      
+  },
+  
+  //somar as saídas
   expenses() {
-    //somar as daídas
+
+    let expense = 0
+    transactions.forEach(transaction => {
+      if (transaction.amount < 0){
+        expense += transaction.amount;
+      }
+    })
+
+    return expense;
+   
   },
 
+  // a soma das entradas e saídas
   total() {
-    // a soma das entradas e saídas
+
+    return Transaction.incomes() + Transaction.expenses();
+      
   },
 };
 
+
 const DOM = {
-    addTransaction(transaction, index) {
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction ()
-    },
+  transactionsContainer: document.querySelector("#data-table tbody"),
 
-    innerHTMLTransaction(){
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
 
-        const html = `    
-        <td class="description">Luz</td>
-        <td class="expense">- R$ 500,00</td>
-        <td class="date">23/01/2021</td>
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `    
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" alt="Remover transação" />
         </td>
-        `
+        `;
 
-        return html
-    }
-}
+    return html;
+  
+  
+  },
 
-DOM.addTransaction()
+//Mostrar os valores atualizados//
+  updateBalance(){
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
+  }
+};
+
+
+//Para modificação dos dados de valor, colocando a moeda e características de positivo ou negativo//
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+
+//Chamar a lista de transações através do for Each substituindo o for//
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
+
+//Chamando a função de atualização dos valores
+ DOM.updateBalance()
